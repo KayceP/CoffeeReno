603f82e6059dd9751375eba5b079a9eef71ac029
KayceP <jedicayce@gmail.com>
2017-09-25 16:56:28 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+![Part of the Udacity Full Stack Web Developer Nanodegree](https://img.shields.io/badge/Udacity-Full%20Stack%20Web%20Developer%20Nanodegree-blue.svg)
+> Developed for project 4 of 5 as part of the **Udacity Full Stack Web Developer Nanodegree**.
+
+# Coffee Reno
+A neighborhood map program made for the [Udacity Full Stack Web Developer Nanodegree Program](https://www.udacity.com/uconnect/intensive/full-stack-web-developer-nanodegree), with Javascript.
+
+![screenshot](https://i.imgur.com/6ey7suP.png)
+
+## Project Overview
+
+For this project, we were tasked with creating a neighborhood map using public API's. I decided to focus mine on the many wonderful coffee offerings Reno has, and added a touch of local flair by utilizing the [UNR](https://www.unr.edu/) colors.
+
+This project utilizes:
+
+- Google Maps API
+- Forismatic API
+- Unsplash Source
+- Javascript
+- KnockoutJS
+- HTML 5
+- CSS3
+
+## User Requirements
+- A modern, up to date browser browser.
+  - [Check yours here!](https://www.whatismybrowser.com/)
+
+## Running
+- For a live demo, click here: [http://kaycep.github.io/Coffee-Reno](http://kaycep.github.io/Coffee-Reno)
+
+ - To run locally, download or clone the repository, then launch the site by double clicking index.html.
+
+## Credits
+- [Google Maps API](https://developers.google.com/maps/)
+- [Google Maps API - Javascript: For Map / Markers / Animation](https://developers.google.com/maps/documentation/javascript/)
+- [Knockout JS](http://knockoutjs.com/documentation/introduction.html)
+- [Forismatic API](https://forismatic.com/en/api/)
+- [Unsplash Source](https://source.unsplash.com/)
+- [JQuery / JQuery Animation Documentation](http://api.jquery.com/)
+- [Max Vargas on Codepen - Dynamic Background & Quote Concept](https://codepen.io/maxslob/pen/bgbOgV) 
+- [Favicon from freefavicon.com](https://www.freefavicon.com/freefavicons/objects/iconinfo/cup-big-flat-152-277132.html)
---
+++ js/core_app.js
@@ -0,0 +1,176 @@
+// Define all variables to satisfy strict mode.
+var document;
+var setTimeout;
+var alert;
+var ko;
+var google;
+
+// Parsing for dynamic background & quote.
+function parseQuote(response) {
+    "use strict";
+    document.getElementById("quote").innerHTML = response.quoteText;
+    document.getElementById("author").innerHTML = "Author - <b>" + response.quoteAuthor + "</b>";
+}
+
+// Specify all locations on map.
+function model() {
+    "use strict";
+    var locations = [{
+        title: "The Hub",
+        lat: 39.521975,
+        lng: -119.822078,
+        id: "The Hub"
+    }, {
+        title: "The Jungle",
+        lat: 39.524982,
+        lng: -119.815983,
+        id: "The Jungle"
+    }, {
+        title: "Bibo Coffee Company",
+        lat: 39.536966,
+        lng: -119.811042,
+        id: "Bibo Coffee Company"
+    }, {
+        title: "Purple Bean",
+        lat: 39.531135,
+        lng: -119.833802,
+        id: "Purple Bean"
+    }, {
+        title: "Sips Coffee and Tea",
+        lat: 39.530438,
+        lng: -119.814742,
+        id: "Sips Coffee and Tea"
+    }];
+    return locations;
+}
+
+var listLocations = ko.observableArray(model());
+
+// Initalize map location & position.
+function initMap() {
+    "use strict";
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 39.529633,
+            lng: -119.813803
+        },
+        zoom: 14
+    });
+
+// Define markers & content.
+    listLocations().forEach(function (data) {
+
+        var positionMk = new google.maps.LatLng(data.lat, data.lng);
+        var marker = new google.maps.Marker({
+            position: positionMk,
+            map: map,
+            title: data.title,
+            animation: google.maps.Animation.DROP
+        });
+
+        var infowindow = new google.maps.InfoWindow({
+            content: data.title
+        });
+
+        data.mapMarker = marker;
+
+        marker.addListener("click", function () {
+            data.triggerMarker(marker);
+            listLocations().forEach(function (place) {
+                if (data.title === place.title) {
+                    place.openInfoWindow();
+                } else {
+                    place.closeInfoWindow();
+                }
+            });
+        });
+
+        map.addListener("click", function () {
+            listLocations().forEach(function (place) {
+                place.closeInfoWindow();
+            });
+        });
+
+        var setMk = function (marker) {
+            infowindow.open(map, marker);
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function () {
+                marker.setAnimation(null);
+            }, 750);
+        };
+        data.triggerMarker = setMk.bind();
+
+        var openMk = function () {
+            infowindow.open(map, marker);
+        };
+        data.openInfoWindow = openMk.bind();
+
+        var closeMk = function () {
+            infowindow.close(map, marker);
+        };
+        data.closeInfoWindow = closeMk.bind();
+
+    });
+
+}
+
+// Define ViewModel for list and sorting of list.
+
+function ViewModel() {
+    "use strict";
+    var self = {};
+
+    self.placeList = ko.observableArray([]);
+
+    listLocations().forEach(function (place) {
+        place.visible = ko.observable(true);
+        self.placeList.push(place);
+    });
+
+    self.filterValue = ko.observable("");
+
+    self.filterList = ko.computed(function () {
+        listLocations().forEach(function (place) {
+            var searchParam = self.filterValue().toLowerCase();
+            var toBeSearched = place.title.toLowerCase();
+
+            place.visible(toBeSearched.indexOf(searchParam) > -1);
+
+            if (place.mapMarker) {
+                place.mapMarker.setVisible(toBeSearched.indexOf(searchParam) > -1);
+            }
+
+            if (place.visible() && searchParam && place.mapMarker) {
+                place.triggerMarker(place.mapMarker);
+            } else if (place.mapMarker) {
+                place.closeInfoWindow();
+            }
+        });
+    });
+
+// Responsiveness for clicking locations on the list.
+    self.onClickListener = function (data) {
+        listLocations().forEach(function (place) {
+            if (data.title === place.title) {
+                place.openInfoWindow();
+            } else {
+                place.closeInfoWindow();
+            }
+        });
+    };
+
+    return self;
+}
+
+ko.applyBindings(new ViewModel());
+
+// Error handling for API's.
+function forismaticError() {
+    "use strict";
+    alert("Forismatic API is unreachable, please check your internet connection and try again.");
+}
+
+function googleMapsError() {
+    "use strict";
+    alert("Google Maps API is unreachable, please check your internet connection and try again.");
+}
